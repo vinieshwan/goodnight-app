1a11b22298a2dcbdf665022e56865275b2f4ed14
Vinia Soldia <vinia.chriszon.soldia@gmail.com>
2023-04-13 05:57:49 +0800

    Create user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+def load_data_from_json(file)
+    file_data = File.read(file)
+    JSON.parse(file_data)
+end
+
+def create_users(users)
+    users.each do |user|
+        User.create(name: user['name'])
+    end
+end
+
+User.delete_all
+create_users(load_data_from_json("#{__dir__}/jsons/users.json"))
